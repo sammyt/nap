ef8088cffe685b116de35324a504e875485826fd
Marcus Stade <marcus@improbable.io>
2014-08-13 19:58:57 +0100

    Removed the via header from forward proxy
    Proxying in general needs more thought, since we'll be proxying across
    domains (webs) more often than not and nap (or rhumb) currently has no
    concept of hosts.

--- proxy/forward.js
+++ proxy/forward.js
@@ -2,10 +2,7 @@ module.exports = forward
 
 function forward(url, handler) {
   return function(req, res) {
-    req.via = [].concat(req.via, req.uri).filter(falsy)
     req.uri = url
     handler(req, res)
   }
 }
-
-function falsy(x) { return !!x }
